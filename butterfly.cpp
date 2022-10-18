3093b8684654e39fcebfc2155acf379328f9ee5a
yashu0123 <88848966+yashu0123@users.noreply.github.com>
2022-10-18 09:55:57 +0530

    Add files via upload

---
+++ butterfly.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row;
+    cin>>row;
+    for(int i=1;i<=row;i++)
+    {    for(int j=1;j<=2*row;j++)
+        {
+            if(i>=j || j>(2*row-i))
+                cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<endl;
+    }
+    for(int i=row;i>0;i--)
+    {    for(int j=1;j<=2*row;j++)
+        {
+            if(i>=j || j>(2*row-i))
+                cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<endl;
+    }
+    
+}
